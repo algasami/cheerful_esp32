47399c868c54bcca7e5ea74a5a4699bb524c5c69
algasami <105358814+algasami@users.noreply.github.com>
2024-07-05 11:07:25 +0800

    added json

--- src/web.cpp
+++ src/web.cpp
@@ -1,4 +1,5 @@
 #include "utils.hpp"
+#include <ArduinoJson.hpp>
 #include <DNSServer.h>
 #include <ESPAsyncWebServer.h>
 #include <LittleFS.h>
@@ -31,7 +32,7 @@ CaptiveRequestHandler captive_handler;
 void setup_web_for_wifi()
 {
     Serial.println("[Setup Web For WiFi] Initializing");
-    web_server.on("/", HTTP_GET, [](AsyncWebServerRequest *req) { req->send(200, "text/plain", "Welcome!"); });
+    web_server.on("/", HTTP_GET, [](AsyncWebServerRequest *req) { req->send(LittleFS, "/index.html"); });
     web_server.on("/setup", HTTP_GET, [](AsyncWebServerRequest *req) { req->send(LittleFS, "/setup.html"); });
     web_server.on("/init", HTTP_POST, [](AsyncWebServerRequest *request) {
         if (!request->hasParam("SSID", true) || !request->hasParam("Password", true))
@@ -43,6 +44,13 @@ void setup_web_for_wifi()
         AsyncWebParameter *pswd = request->getParam("Password", true);
         request->send(200, "text/plain", "Sent...");
         set_wifi(ssid->value(), pswd->value());
+    });
+    web_server.on("/status", HTTP_GET, [](AsyncWebServerRequest *req) {
+        ArduinoJson::JsonDocument status;
+        status["wifi_connected"] = bWiFiConnected;
+        char serialize_buffer[100];
+        ArduinoJson::serializeJson(status, serialize_buffer);
+        req->send(200, "application/json", serialize_buffer);
     });
     web_server.addHandler(&captive_handler);
     web_server.begin();
